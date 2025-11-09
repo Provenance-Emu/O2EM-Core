7e47faa78877510994add60bfee4c1437950ddd0
Joseph Mattiello <git@joemattiello.com>
2025-11-08 22:46:43 -0500

    add numpad buttons
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- PVOdysseyGameCore/OdysseyGameCore+Controls.m
+++ PVOdysseyGameCore/OdysseyGameCore+Controls.m
@@ -51,6 +51,26 @@ int joystick_data[2][5]={{0,0,0,0,0},{0,0,0,0,0}};
         joystick_data[player][3] = 1;
     else if (button == PVOdyssey2ButtonAction)
         joystick_data[player][4] = 1;
+    else if (button == PVOdyssey2ButtonKey0)
+        key[RETROK_0] = 1;
+    else if (button == PVOdyssey2ButtonKey1)
+        key[RETROK_1] = 1;
+    else if (button == PVOdyssey2ButtonKey2)
+        key[RETROK_2] = 1;
+    else if (button == PVOdyssey2ButtonKey3)
+        key[RETROK_3] = 1;
+    else if (button == PVOdyssey2ButtonKey4)
+        key[RETROK_4] = 1;
+    else if (button == PVOdyssey2ButtonKey5)
+        key[RETROK_5] = 1;
+    else if (button == PVOdyssey2ButtonKey6)
+        key[RETROK_6] = 1;
+    else if (button == PVOdyssey2ButtonKey7)
+        key[RETROK_7] = 1;
+    else if (button == PVOdyssey2ButtonKey8)
+        key[RETROK_8] = 1;
+    else if (button == PVOdyssey2ButtonKey9)
+        key[RETROK_9] = 1;
 }
 
 - (void)didReleaseOdyssey2Button:(PVOdyssey2Button)button forPlayer:(NSInteger)player {
@@ -65,6 +85,26 @@ int joystick_data[2][5]={{0,0,0,0,0},{0,0,0,0,0}};
         joystick_data[player][3] = 0;
     else if (button == PVOdyssey2ButtonAction)
         joystick_data[player][4] = 0;
+    else if (button == PVOdyssey2ButtonKey0)
+        key[RETROK_0] = 0;
+    else if (button == PVOdyssey2ButtonKey1)
+        key[RETROK_1] = 0;
+    else if (button == PVOdyssey2ButtonKey2)
+        key[RETROK_2] = 0;
+    else if (button == PVOdyssey2ButtonKey3)
+        key[RETROK_3] = 0;
+    else if (button == PVOdyssey2ButtonKey4)
+        key[RETROK_4] = 0;
+    else if (button == PVOdyssey2ButtonKey5)
+        key[RETROK_5] = 0;
+    else if (button == PVOdyssey2ButtonKey6)
+        key[RETROK_6] = 0;
+    else if (button == PVOdyssey2ButtonKey7)
+        key[RETROK_7] = 0;
+    else if (button == PVOdyssey2ButtonKey8)
+        key[RETROK_8] = 0;
+    else if (button == PVOdyssey2ButtonKey9)
+        key[RETROK_9] = 0;
 }
 
 @end
