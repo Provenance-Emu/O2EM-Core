576c55ec56a5dd0a2c9def064996664b25d24e58
Joseph Mattiello <git@joemattiello.com>
2026-03-04 23:28:06 -0500

    build: set iOS/tvOS minimum to v17
    Signed-off-by: Joseph Mattiello <git@joemattiello.com>

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ import PackageDescription
 let package = Package(
     name: "PVVirtualJaguar",
     platforms: [
-        .iOS(.v13),
-        .tvOS(.v13),
+        .iOS(.v17),
+        .tvOS(.v17),
         .watchOS(.v9),
         .macOS(.v10_13),
-        .macCatalyst(.v14)
+        .macCatalyst(.v17)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
